38c083c8ad806befe14389a2c7d1094d468f6e9e
Johnnie843 <cooperj2@g.cofc.edu>
2019-01-23 22:54:55 -0500

    Initial Commit of My First Andriod Project, adding andriod file structure

---
+++ app/src/main/java/com/example/johnniecooper/first_android_app_cooper/MyFirstAndriodAppActivity.java
@@ -0,0 +1,14 @@
+-a
+        package com.example.johnniecooper.first_android_app_cooper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyFirstAndriodAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_first_andriod_app);
+    }
+}
