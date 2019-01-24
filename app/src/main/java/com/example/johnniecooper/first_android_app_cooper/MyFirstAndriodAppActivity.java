bd9bb2138cfa35addb7093d44c8cd29222eeea8e
Johnnie843 <cooperj2@g.cofc.edu>
2019-01-23 23:19:40 -0500

    Completed all the Chapter 3 Steps

--- app/src/main/java/com/example/johnniecooper/first_android_app_cooper/MyFirstAndriodAppActivity.java
+++ app/src/main/java/com/example/johnniecooper/first_android_app_cooper/MyFirstAndriodAppActivity.java
@@ -1,14 +1,26 @@
--a
-        package com.example.johnniecooper.first_android_app_cooper;
+package com.example.johnniecooper.first_android_app_cooper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import com.example.johnniecooper.first_android_app_cooper.R;
 
 public class MyFirstAndriodAppActivity extends AppCompatActivity {
+
+    public  static final String DEBUG_TAG = "MyFirstAppLogging";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_first_andriod_app);
+        Log.i(DEBUG_TAG,"In the onCreate method of the MyFirstAndriodAppActivity Class" );
+        //forceError();
+    }
+
+
+    public void forceError(){
+        if(true){
+            throw new Error("Whoops");
+        }
     }
 }
